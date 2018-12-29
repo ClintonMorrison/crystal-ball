8daa0ed3a2c2658edd4d272ba26677bc0eea2d67
Clinton Morrison <contact@clintonmorrison.com>
2018-12-28 22:29:05 -0400

    use less memory when evaluating ngram models

--- README.md
+++ README.md
@@ -9,6 +9,8 @@ Usage of ./stockAnalysis:
         Download list of current companies
   -download-quotes
         Download weekly stock price data
+  -evaluate-ngram-model-over-time
+        Evaluates an ngram model with n=3 over time
   -evaluate-ngram-model-params
         Evaluate different parameters for building n-gram models
   -predict
@@ -16,5 +18,5 @@ Usage of ./stockAnalysis:
   -print-ngrams
         Prints n-gram frequencies for aggregate stock data
   -start-ticker string
-        Ticker to start at
+        Flag specifying ticker to start at (for -download-quotes)
 ```
--- evaluateNGramModelOverTime.go
+++ evaluateNGramModelOverTime.go
@@ -6,30 +6,24 @@ import (
 
 func EvaluateNGramModelOverTime () {
 	maxN := 3
-	splitDate := ParseDateString("2000-01-01")
+	splitDate := ParseDateString("2016-01-01")
 	endDate := ParseDateString("2018-10-28")
-
-	resultsByConfiguration := make(map[string]*EvaluationResults, 0)
+	company := Company{}
 
 	db := GetHandle()
 
-	tickers := GetAllCompanyTickers(db)
+	tickers := GetAllCompanyTickers(db)[:1]
 	dates := make([]string, 0)
 
 	fmt.Print(tickers)
 
-	companies := make(map[string]*Company, 0)
-	for _, ticker := range tickers {
-		company := GetCompanyByTicker(db, ticker)
-		companies[ticker] = company
-	}
+	fmt.Printf("\n\n\n%10s\t%10s\t%s\n", "Index", "Date", "Performance")
 
+	i := 1
 
 	for splitDate.Before(endDate) {
 		dates = append(dates, TimeToString(splitDate))
-		splitDate = AddDays(splitDate, 7)
-		fmt.Println(TimeToString(splitDate))
-		fmt.Printf("Building model for %s\n", TimeToString(splitDate))
+		splitDate = AddDays(splitDate, 28)
 
 		trainingDocuments := make(map[string]string, 0)
 		evaluationDocuments := make(map[string]string, 0)
@@ -42,38 +36,28 @@ func EvaluateNGramModelOverTime () {
 		}
 
 		ngramModel := CreateNGramModel(GradeQuoteClassifier, "", maxN)
-		key := TimeToString(splitDate)
 		evaluationResults := EvaluationResults{}
 		evaluationResults.init()
-		resultsByConfiguration[key] = &evaluationResults
 
-		for ticker, document := range trainingDocuments {
-			ngramModel.AddCase(companies[ticker], document)
+		for _, document := range trainingDocuments {
+			ngramModel.AddCase(&company, document)
 		}
 
 		for ticker, document := range evaluationDocuments {
-			company := companies[ticker]
 			trainingDocument := trainingDocuments[ticker]
 			trainingEnd := len(trainingDocument)
 			nextChar := trainingEnd + 1
 			if len(trainingDocument) < maxN || len(document) < len(trainingDocument) + 1  {
-				fmt.Printf("[WARN] skipping %s because not enough training data\n", ticker)
+				// fmt.Printf("[WARN] skipping %s because not enough training data\n", ticker)
 				continue
 			}
 
-			pred, _ := ngramModel.PredictNext(company, trainingDocument)
+			pred, _ := ngramModel.PredictNext(&company, trainingDocument)
 			actual := document[trainingEnd:nextChar]
 			evaluationResults.AddCase(actual, pred)
 		}
-	}
 
-	i := 0
-	fmt.Printf("\n\n\n%10s\t%10s\t%s\n", "Index", "Date", "Performance")
-	for _, date := range dates {
-		results := resultsByConfiguration[date]
-		if results != nil {
-			i++
-			fmt.Printf("%10d\t%10s\t%2.2f%%\n", i, date, results.PercentCorrect())
-		}
+		fmt.Printf("%10d\t%10s\t%2.2f%%\n", i, TimeToString(splitDate), evaluationResults.PercentCorrect())
+		i++
 	}
 }
